a5cb2edce3f2879763373016a554c1b8d187454f
huangshaofei <1152594956@qq.com>
2021-09-29 11:04:20 +0800

    查看应用名称

--- app/src/main/java/com/howfor/mydemolist/MainActivity.kt
+++ app/src/main/java/com/howfor/mydemolist/MainActivity.kt
@@ -2,6 +2,7 @@ package com.howfor.mydemolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 
@@ -11,8 +12,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnHello = findViewById<Button>(R.id.btn_hello)
+        val btnAppName = findViewById<Button>(R.id.btn_app_name)
+
         btnHello.setOnClickListener {
             Toast.makeText(this, "世界你好啊！", Toast.LENGTH_SHORT).show()
+        }
+        btnAppName.setOnClickListener {
+            val pm = packageManager
+            val applicationInfo = pm.getApplicationInfo("com.howfor.invokedemo", 0)
+            val appName = pm.getApplicationLabel(applicationInfo)
+            Log.d("daisy", "appName $appName")
         }
     }
 }
