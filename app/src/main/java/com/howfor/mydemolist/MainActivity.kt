fe648981f15aefeadfcad2839e0055acaf3662ed
huangshaofei <1152594956@qq.com>
2021-09-29 10:47:31 +0800

    MainActivity的提交

--- app/src/main/java/com/howfor/mydemolist/MainActivity.kt
+++ app/src/main/java/com/howfor/mydemolist/MainActivity.kt
@@ -3,6 +3,7 @@ package com.howfor.mydemolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnHello = findViewById<Button>(R.id.btn_hello)
+        btnHello.setOnClickListener {
+            Toast.makeText(this, "世界你好啊！", Toast.LENGTH_SHORT).show()
+        }
     }
 }
