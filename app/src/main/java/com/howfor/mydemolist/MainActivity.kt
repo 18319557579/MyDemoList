bfe81387fe0a6a1da0515f9e076468ab6bb5bda4
huangshaofei <1152594956@qq.com>
2021-09-29 09:18:57 +0800

    修改Hello World！

--- app/src/main/java/com/howfor/mydemolist/MainActivity.kt
+++ app/src/main/java/com/howfor/mydemolist/MainActivity.kt
@@ -2,10 +2,13 @@ package com.howfor.mydemolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnHello = findViewById<Button>(R.id.btn_hello)
     }
 }
